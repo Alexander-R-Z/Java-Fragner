32f5492c74c44215cab8b285fa2bb76b058798e9
Alexander Riccardo Zimmermann <100205235+Alexander-R-Z@users.noreply.github.com>
2023-09-28 10:32:31 +0200

    Teilen mit int und double

---
+++ Teilen_mit_int_und_double.java
@@ -0,0 +1,12 @@
+public class Teilen_mit_int_und_double {
+    public static void main (String[] args) {
+        int a=7, b=5;
+        double ad=7, bd=5;
+        if (a/b==ad/bd) {
+            System.out.println(a/b+" = a/b==ad/bd = "+ad/bd+" int a="+a+" / int b"+b+" ist gleich double ad="+ad+" / double bd="+bd);
+        }
+        else {
+            System.out.println(a/b+" = a/b!=ad/bd = "+ad/bd+" int a="+a+" / int b"+b+" ist nicht gleich double ad="+ad+" / double bd="+bd);
+        }
+    }
+}
